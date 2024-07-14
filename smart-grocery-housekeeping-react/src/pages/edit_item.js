4f27a5a787e969b14bd5bbc75d29b19ae2bf85ec
Kevin Klein <kevin.klein@chghealthcare.com>
2024-07-13 18:26:23 -0600

    merging main info branch

--- smart-grocery-housekeeping-react/src/pages/edit_item.js
+++ smart-grocery-housekeeping-react/src/pages/edit_item.js
@@ -1,21 +1,28 @@
 import React from "react";
+import { FaImage } from "react-icons/fa";
  
 const Edit_Item = () => {
     return (
         <div class="core">
-            <h2>
-                Grocery Item Name
-            </h2>
-            <p>Tags go here</p>
-            <p>Projected spoil date: DATE</p>
-            <p>Current quantity - Unit of measure</p>
-            <br/>
-            <label for="fname">Update Quantity</label> <br/>
-            <input type="number" id="quantity" name="quantity" value="1"/> <br/>
-            <input type="submit" value="Spoiled"></input>
-            <input type="submit" value="Finished"></input>
-            <br/>
-            <a href="/">Return to Dashboard</a>
+            <div class="left-inline-div">
+                <FaImage class="image-placeholder"/>
+            </div>
+            <div class="right-inline-div">
+                <h2>
+                    Grocery Item Name
+                </h2>
+                <p>Tags go here</p>
+                <p>Projected spoil date: DATE</p>
+                <p>Current quantity - Unit of measure</p>
+                <br/>
+                <label for="fname">Update Quantity</label> <br/>
+                <input type="number" id="quantity" name="quantity" value="1"/> <br/>
+                <input type="submit" value="Spoiled"></input>
+                <input type="submit" value="Finished"></input>
+                <br/>
+                <a href="/">Return to Dashboard</a>
+            </div>
+            
         </div>
     );
 };
