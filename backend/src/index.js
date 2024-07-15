284b63a2a2e13137515d44a9b234779df64ce6e1
Kevin Klein <kevin.klein@chghealthcare.com>
2024-07-15 11:23:56 -0600

    added initial database setup to test that we can connect and query it, updated gitignore to ignore .env files in frontend and backend folders

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const express = require('express');
 const { Client } = require('pg');
 const app = express();
@@ -29,3 +28,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = client;
