3be26c1a114231fa60b14f619d699a6e43ef4964
rocovington <rocovington9@gmail.com>
2024-07-19 01:35:56 -0400

    still need to account for error, but page waits to load so that attribute can be called out, will add mapping of vals and errors next and remove extra attempt,  wip

--- frontend/src/pages/view_recipe.js
+++ frontend/src/pages/view_recipe.js
@@ -3,25 +3,44 @@ import{ useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
 
+
 const View_Recipe = () => {
 
     const [steps, setSteps] = useState([]);
+    const [error, setError] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const fetchSteps = () => {
-        fetch('http://localhost:3001/api/recipes/1/steps')
-        .then(response => {
-            return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        setSteps(data);
-    })
-    }
+    
+    useEffect(() => {
+        const fetchStepData = async () => {
+            setLoading(true);
+            const stepRes  = await fetch('http://localhost:3001/api/recipes/1/steps');
+            const stepData = await stepRes.json();
+            setSteps(stepData);
+            setLoading(false);
+        }
+    
+    fetchStepData();}, []);
 
-useEffect(() => {
-    fetchSteps()}
-    , []);
+//    const [steps, setSteps] = useState([]);
+//
+//    const fetchSteps = () => {
+//        fetch('http://localhost:3001/api/recipes/1/steps')
+//        .then(response => {
+//            return response.json();
+//    })
+//    .then(data => {
+//        console.log(data);
+//        setSteps(data);
+//    })
+//    }
 
+//useEffect(() => {
+//    fetchSteps()}
+//    , []);
+
+    //to get rid of error where attribute set before assigned
+    if (loading) return <h1>Loading</h1>;
 
     return (
         <div class="core">
@@ -30,25 +49,24 @@ useEffect(() => {
             </h1>
             <p>Recipe Description</p>
             <h2>Ingredients</h2>
-            <p>
                 <ul>
                     <li>item 1</li>
                     <li>item 2</li>
                     <li>item 3</li>
                     <li>item 4</li>
                 </ul>
-            </p>
             <h2>Recipe Steps</h2>
-            <p>
+
                 <ol>
-                    <li> step one instructions</li>
-                    <li> step two instructions</li>
-                    <li> step three instructions</li>
-                    <li> step four instructions</li>
+                    <li> {steps[1].stepdescription}</li>
+                    <li> {steps[3].stepdescription}</li>
+                    <li> {steps[4].stepdescription}</li>
+                    <li> {steps[5].stepdescription}</li>
                 </ol>
-            </p>
+
         </div>
-    );
+    )
 };
+
  
 export default View_Recipe;
