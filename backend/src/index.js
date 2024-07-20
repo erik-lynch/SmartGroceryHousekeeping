b8f801a7dea5ee75599e2428753f77063760a86f
Kevin Klein <kevin.klein@chghealthcare.com>
2024-07-20 15:24:15 -0600

    Add items by clicking submit on add items page, database updates successfully

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const { Pool } = require('pg');
 const app = express();
 
@@ -16,6 +18,16 @@ pool.connect()
   .then(() => console.log('Connected to the database'))
   .catch(err => console.error('Connection error', err.stack));
 
+app.use(cors({
+  origin: 'http://localhost:3001', 
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+app.use(bodyParser.json());
+
+
 app.get('/items', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM items');
@@ -23,6 +35,35 @@ app.get('/items', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send('Server error');
+  }
+});
+
+app.post('/api/add-item', async (req, res) => {
+  const { iname, unit, quantity, ripeRating, barcode, itemDescription, recipeId } = req.body; 
+
+  try {
+    const itemResult = await pool.query(
+      'SELECT itemId FROM Items WHERE itemName = $1 OR itemName = $2',
+      [iname, barcode]
+    );
+
+    let itemId;
+
+    if (itemResult.rows.length > 0) {
+      // Item exists
+      itemId = itemResult.rows[0].itemid;
+    } else {
+      // Insert new item
+      const insertItemResult = await pool.query(
+        'INSERT INTO Items (itemName, itemDescription) VALUES ($1, $2) RETURNING itemId',
+        [iname, itemDescription] 
+      );
+      itemId = insertItemResult.rows[0].itemid;
+    }
+    res.status(200).json({ message: 'Item added successfully' });
+  } catch (error) {
+    console.error('Error adding item:', error); 
+    res.status(500).json({ message: 'Error adding item', error: error.message });
   }
 });
 
