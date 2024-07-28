56795130ee3be3a4fc2a3403a79e98483dbd2a3c
Jillian Vondy <vondyj@oregonstate.edu>
2024-07-28 10:33:02 -0700

    Merged CSS into one file

--- frontend/src/components/Carousel/ItemCarousel.js
+++ frontend/src/components/Carousel/ItemCarousel.js
@@ -2,7 +2,7 @@ import { React, Component } from "react";
 import { Link } from 'react-router-dom';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import './Carousel.css'
+
 
 const responsive = {
   desktop: {
