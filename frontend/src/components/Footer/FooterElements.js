306dd6ee55e83a49404c07c99674bc00ce1ac52b
Jillian Vondy <vondyj@oregonstate.edu>
2024-07-30 09:37:55 -0700

    Fixed button issues from CSS merge

--- frontend/src/components/Footer/FooterElements.js
+++ frontend/src/components/Footer/FooterElements.js
@@ -4,7 +4,7 @@ export const Box = styled.div`
     
     background: #FFE7C3;
     border-top: solid 1px black;
-    bottom: 0px;
+    bottom: 0;
     width: 100%;
     height: 200px;
     margin-top: 50px;
