509b1d32141e85ba801b0ea660bfcd24c32d4dc9
kevinlucasklein <kleinkev@oregonstate.edu>
2024-08-10 21:26:04 -0600

    removing api key logs from add items

--- frontend/src/pages/add_item.js
+++ frontend/src/pages/add_item.js
@@ -32,9 +32,7 @@ const Add_Item = () => {
     preview: '',
     data: ''
   });
-  console.log('license key1', process.env.REACT_APP_SCANDIT_LICENSE_KEY);
   const licenseKey = process.env.REACT_APP_SCANDIT_LICENSE_KEY.replace(/['"]/g, '');
-  console.log('license key2', licenseKey);
 
   // temporary for demo
   const userId = 1;
